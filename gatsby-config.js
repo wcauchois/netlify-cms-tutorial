3b6e717c3269b54868e90c2303a10249f47a368c
Bill Cauchois <bill@tryfi.com>
2020-02-17 20:58:44 -0500

    add index and blog post rendering and such

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,13 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  plugins: [`gatsby-plugin-netlify-cms`],
-}
+  plugins: [
+    `gatsby-plugin-netlify-cms`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: 'markdown-pages',
+        path: `${__dirname}/blog`
+      }
+    },
+    `gatsby-transformer-remark`
+  ],
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,43 @@
 import React from "react"
+import { graphql, Link } from "gatsby"
 
-export default () => <div>Hello world!</div>
+export default function Index({ data }) {
+  const edges = data.allMarkdownRemark.edges;
+  return (
+    <div>
+      <h1>
+        Yo... welcome
+      </h1>
+      <p>
+        Imagine some pretty interface here.
+      </p>
+      <div>
+        {edges.map(({ node }) => (
+          <div>
+            <h2>
+              <Link to={node.frontmatter.path}>
+                {node.frontmatter.title}
+              </Link>
+            </h2>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export const pageQuery = graphql`
+  {
+    allMarkdownRemark {
+      edges {
+        node {
+          frontmatter {
+            path
+            title
+            date
+          }
+        }
+      }
+    }
+  }
+`
